4527ed5e8e3e881f6aee1c6c804e3602590c6ba3
dany92 <dany@bc.edu>
2016-02-25 10:54:06 -0600

    Create homepage and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   #post 'users', to: 'users#create'
+  root 'pages#home'
   resources :users, except: [:new]
   resources :articles
 end
